711049a12aa62d8bff7650c404c2e919e91f0656
Aayush-Tiwari-NucleusTeq <aayush.tiwari@nucleusteq.com>
2023-07-23 20:59:12 +0530

    Second assign done

---
+++ Day_3/Invalid_Input_Exception/Example.java
@@ -0,0 +1,7 @@
+package Day_3.Invalid_Input_Exception;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("I am the example of Invalid Input Exception");
+    }
+}
