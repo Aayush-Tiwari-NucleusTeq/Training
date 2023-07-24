38caa0674d7c686451478b80ba9a97208fb9d19b
Aayush-Tiwari-NucleusTeq <aayush.tiwari@nucleusteq.com>
2023-07-24 11:32:39 +0530

    Added ATM task

--- Day_3/Invalid_Input_Exception/Example.java
+++
@@ -1,7 +0,0 @@
-package Day_3.Invalid_Input_Exception;
-
-public class Example {
-    public static void main(String[] args) {
-        System.out.println("I am the example of Invalid Input Exception");
-    }
-}
---
+++ Day_3/Invalid_Input_Exception/InvalidInputException.java
@@ -0,0 +1,7 @@
+package Day_3.Invalid_Input_Exception;
+
+public class InvalidInputException extends Exception{
+       public InvalidInputException(String message){
+        super(message);
+       }
+}
