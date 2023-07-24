9c23f98422ba85d3f62215088b81ac11ecaac35d
Aayush-Tiwari-NucleusTeq <aayush.tiwari@nucleusteq.com>
2023-07-24 17:07:05 +0530

    Updated content

---
+++ Day_3/File_Copy/FileCopy.java
@@ -0,0 +1,7 @@
+package Day_3.File_Copy;
+
+public class FileCopy {
+    public static void main(String[] args) {
+        System.out.println("Program illustrating copy content of a file to another one");
+    }
+}
--- Day_3/File_Read/ReadFile.java
+++ Day_3/File_Read/ReadFile.java
@@ -1,7 +1,28 @@
 package Day_3.File_Read;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ReadFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Program illustrating reading of file");
+        try {
+            System.out.println("Enter the file name you want to print");
+            String name = sc.nextLine();
+            FileReader fileReader = new FileReader("D:\\Training\\Day_3\\File_Read\\" + name + ".txt");
+            int i;
+            while ((i = fileReader.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found exception block");
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOExceptoin block");
+            System.out.println(e.getLocalizedMessage());
+        }
     }
 }
