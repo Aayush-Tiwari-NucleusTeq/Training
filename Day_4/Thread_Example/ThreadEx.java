d23639952dd924b8f29416ac9df9783d83722959
Aayush-Tiwari-NucleusTeq <aayush.tiwari@nucleusteq.com>
2023-07-25 14:39:47 +0530

    First commit on day 4 task

---
+++ Day_4/Thread_Example/ThreadEx.java
@@ -0,0 +1,7 @@
+package Day_4.Thread_Example;
+
+public class ThreadEx {
+    public static void main(String[] args) {
+        System.out.println("Program illustrating the use of threads in java");
+    }
+}
