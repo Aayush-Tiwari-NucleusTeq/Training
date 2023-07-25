b264b88a82f32e22e9f3e01d2eeffaaac6d735aa
Aayush-Tiwari-NucleusTeq <aayush.tiwari@nucleusteq.com>
2023-07-25 14:41:27 +0530

    Added another task on Multithreading

---
+++ Day_4/Running_Thread/CheckRunningThread.java
@@ -0,0 +1,7 @@
+package Day_4.Running_Thread;
+
+public class CheckRunningThread {
+    public static void main(String[] args) {
+        System.out.println("Program illustrating check all running threads in a program");
+    }
+}
