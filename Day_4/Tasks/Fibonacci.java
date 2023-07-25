80b8cde2f7761d0dc289d61cc04872596dfc5abc
Aayush-Tiwari-NucleusTeq <aayush.tiwari@nucleusteq.com>
2023-07-25 17:50:44 +0530

    Added fibonacci task in all tasks question

--- Day_4/Tasks/Fibonacci.java
+++ Day_4/Tasks/Fibonacci.java
@@ -1,11 +1,23 @@
 package Day_4.Tasks;
 
-public class Fibonacci extends Thread{
+public class Fibonacci extends Thread {
     int num;
-    Fibonacci(int num){
+
+    Fibonacci(int num) {
         this.num = num;
     }
-    public void run(){
-        System.out.println("I am fibonacci thread " + num);
+
+    public void run() {
+        int n1 = 0, n2 = 1, n3, i, count = num;
+        System.out.print(n1 + " " + n2);
+        for (i = 2; i < count; ++i)
+        {
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+        System.out.println();
+        // System.out.println("I am fibonacci thread " + num);
     }
 }
