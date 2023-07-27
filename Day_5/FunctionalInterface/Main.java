aa4351c042f340e0f6095848c095d5c7152b7708
Aayush-Tiwari-NucleusTeq <aayush.tiwari@nucleusteq.com>
2023-07-27 16:19:14 +0530

    Modified functional interface task

--- Day_5/FunctionalInterface/Main.java
+++ Day_5/FunctionalInterface/Main.java
@@ -28,19 +28,19 @@ public class Main {
             return 2 * PI * 8 * (8 + 12);
         };
 
-        var circle_area = circle.area();
-        var rectangle_area = rectangle.area();
-        var square_area = square.area();
-        var cube_area = cube.area();
-        var sphere_area = sphere.area();
-        var cylinder_area = cylinder.area();
+        // var circle_area = circle.area();
+        // var rectangle_area = rectangle.area();
+        // var square_area = square.area();
+        // var cube_area = cube.area();
+        // var sphere_area = sphere.area();
+        // var cylinder_area = cylinder.area();
 
-        System.out.println("The area of circle is -> " + circle_area);
-        System.out.println("The area of rectangle is -> " + rectangle_area);
-        System.out.println("The area of square is -> " + square_area);
-        System.out.println("The area of cube is -> " + cube_area);
-        System.out.println("The area of sphere is -> " + sphere_area);
-        System.out.println("The area of cylinder is -> " + cylinder_area);
+        System.out.println("The area of circle is -> " + circle.area());
+        System.out.println("The area of rectangle is -> " + rectangle.area());
+        System.out.println("The area of square is -> " + square.area());
+        System.out.println("The area of cube is -> " + cube.area());
+        System.out.println("The area of sphere is -> " + sphere.area());
+        System.out.println("The area of cylinder is -> " + cylinder.area());
 
     }
 }
