d758aedbc95c25c72056ac0b8c7fe7ab0afeadd2
Aayush-Tiwari-NucleusTeq <aayush.tiwari@nucleusteq.com>
2023-07-27 17:52:43 +0530

    Added another task

---
+++ Day_6/SubclassesIOStream/Main.java
@@ -0,0 +1,7 @@
+package SubclassesIOStream;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program illustrating working with all the subclasses available in Input Output Stream in Java");
+    }
+}
