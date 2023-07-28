4dc09d85ec24308f92af18c9e2b2092600c0613d
Aayush-Tiwari-NucleusTeq <aayush.tiwari@nucleusteq.com>
2023-07-28 12:02:19 +0530

    Added filter output stream task

---
+++ Day_6/FilterInputStream/Main.java
@@ -0,0 +1,12 @@
+package FilterInputStream;
+
+class MyFile{
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program illustrating filter output stream in java");
+
+    }
+}
